a74255852b14ff5626fc27e4666933dfb00bcdb7
Toby Cotton <94650770+TobyCotton@users.noreply.github.com>
2024-04-16 12:14:14 +0100

    working perlin

--- Assets/ProceduralScripts/TerrainPerlin/PerlinNoiseTerrain.cs
+++ Assets/ProceduralScripts/TerrainPerlin/PerlinNoiseTerrain.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using TreeEditor;
 using UnityEngine;
 
-public static class PerlinNoiseTerrain
+public class PerlinNoiseTerrain
 {
-    public static float[,] GenerateNoise(int width,int height, float scale,int octaves, float persistance, float octaveIncrease,int seed, Vector2 offset)
+    public static float[,] GenerateNoise(int width,int height, float scale,int octaves, float persistance, float octaveIncrease,int seed, Vector2 offset, int[] Permutation)
     {
         float[,] heights = new float[width,height];
         Vector2[] octaveOffsets = new Vector2[octaves];
@@ -43,7 +43,7 @@ public static class PerlinNoiseTerrain
                     float sampleX = (x- halfW) / (scale * frequency) + octaveOffsets[i].x;
                     float sampleY = (y-halfH) / (scale * frequency) + octaveOffsets[i].y;
 
-                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 -1;
+                    float perlinValue = Noise2D(sampleX, sampleY, Permutation) * 2 -1;//Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                     noiseHeight += perlinValue * amplitude;
                     amplitude *= persistance;
                     frequency *= octaveIncrease;
@@ -69,5 +69,66 @@ public static class PerlinNoiseTerrain
         }
 
         return heights;
+    }
+    public static float Noise2D(float x, float y, int[] Permutation)
+    {
+        int X = (int)Math.Floor(x) & 255;
+        int Y = (int)Math.Floor(y) & 255;
+
+        float xf = x - (float)Math.Floor(x);
+        float yf = y - (float)Math.Floor(y);
+
+        Vector2 topRight = new Vector2(xf - 1.0f, yf - 1.0f);
+        Vector2 topLeft = new Vector2(xf, yf - 1.0f);
+        Vector2 bottomRight = new Vector2(xf - 1.0f, yf);
+        Vector2 bottomLeft = new Vector2(xf, yf);
+
+        int valueTopRight = Permutation[(Permutation[(X + 1)&255] + Y + 1) & 255];
+        int valueTopLeft = Permutation[(Permutation[X] + Y + 1) & 255];
+        int valueBottomRight = Permutation[(Permutation[(X + 1) & 255] + Y) & 255];
+        int valueBottomLeft = Permutation[(Permutation[X] + Y) & 255];
+
+        int temp1 = (int)valueTopRight&3;
+        int temp2 = (int)valueTopLeft & 3;
+        int temp3 = (int)valueBottomRight & 3;
+        int temp4 = (int)valueBottomLeft & 3;
+
+        Vector2 topRightVector = SelectCorner(temp1);
+        Vector2 topLeftVector = SelectCorner(temp2);
+        Vector2 bottomRightVector = SelectCorner(temp3);
+        Vector2 bottomLeftVector = SelectCorner(temp4);
+
+        float dotTopRight = (topRight.x * topRightVector.x) + (topRight.y * topRightVector.y);
+        float dotTopLeft = (topLeft.x * topLeftVector.x) + (topLeft.y * topLeftVector.y);
+        float dotBottomRight = (bottomRight.x * bottomRightVector.x) + (bottomRight.y * bottomRightVector.y);
+        float dotBottomLeft = (bottomLeft.x * bottomLeftVector.x) + (bottomLeft.y * bottomLeftVector.y);
+
+        float u = ((6 * xf - 15) * xf + 10) * xf * xf * xf;
+        float v = ((6 * yf - 15) * yf + 10) * yf * yf * yf;
+
+        return LerpMine(u,
+            LerpMine(v, dotBottomLeft, dotTopLeft),
+            LerpMine(v, dotBottomRight, dotTopRight)
+        );
+    }
+    private static Vector2 SelectCorner(int c)
+    {
+        switch (c)
+        {
+            case 0:
+                return new Vector2(1.0f, 1.0f);
+            case 1:
+                return new Vector2(-1.0f, 1.0f);
+            case 2:
+                return new Vector2(-1.0f, -1.0f);
+            case 3:
+                return new Vector2(1.0f, -1.0f);
+        }
+
+        return new Vector2(-2.0f, -2.0f);
+    }
+    private static float LerpMine(float a, float b, float c)
+    {
+        return b + a * (c - b);
     }
 }
